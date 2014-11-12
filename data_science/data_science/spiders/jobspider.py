e268e8d6b30ca275974bd5daee77fdc7630d0bf3
theresilientdeveloper <theresilientdeverloper@gmail.com>
2014-11-12 12:03:22 -0800

    site spider unable to crawl
    completed site spider, but was banned (perhaps) . . should breakdown
    different category

--- data_science/data_science/spiders/jobspider.py
+++ data_science/data_science/spiders/jobspider.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
-from scrapy.contrib.linkextractors import LinkExtractor
+from scrapy.contrib.spiders import CrawlSpider
 from data_science.items import DataScienceItem
-from data_science.spiders import methods
 
 class JobSpider(scrapy.Spider):
     """
@@ -19,34 +18,15 @@ class JobSpider(scrapy.Spider):
     #rules = (Rule(SgmlLinkExtractor(allow=("jobs", ),)
     #, callback="parse_items", follow= True),
     #)
-    print methods.get_category()
     def parse(self, response):
 
         items = []
-
-
         item = DataScienceItem()
         item['title'] =  response.xpath('//title/text()').extract() #title of the page
         item['text_span'] =  response.xpath('//span/text()').extract() #span class, usually all the texts
         item['text_li'] =  response.xpath('//li/text()').extract() #bullet points in the texts
         items.append(item)
 
-
         filename = "datascience.txt"
         with open(filename, 'wb') as f:
             f.write(str(items))
-
-
-class SiteSpider(scrapy.Spider):
-    """
-    spider for the site analytictalent.com
-    """
-    name = "jobspider"
-    allowed_domains = ["analytictalent.com"]
-    start_urls = (
-        'http://www.analytictalent.com/',
-        )
-
-    #rules = (Rule(SgmlLinkExtractor(allow=("jobs", ),)
-    #, callback="parse_items", follow= True),
-    #)
--- data_science/data_science/spiders/methods.py
+++ data_science/data_science/spiders/methods.py
@@ -1,16 +1,43 @@
 import scrapy
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from data_science.items import DataScienceItem
+from scrapy.contrib import linkextractors
+#def get_category():
+#    #first, get all the anchor tags
+#    scrapy.fetch("http://www.analytictalent.com/")
+#    anchor = response.xpath('//a/@href').extract()
+#    #then, extract all category out
+#    category  = [s.replace("http://careers.analytictalent.com/jobs/browse/category/","") for s in anchor if "category" in s]
+#    top = "http://careers.analytictalent.com/jobs/search/results?category%5B0%5D="
+#    bot = "&format=rss"
+#    url = []
+#    for cat in category:
+#        url.append(top+cat+url)
+#
+#    return url
+#    #
 
-def get_category():
-    #first, get all the anchor tags
-    scrapy.fetch("http://www.analytictalent.com/")
-    anchor = response.xpath('//a/@href').extract()
-    #then, extract all category out
-    category  = [s.replace("http://careers.analytictalent.com/jobs/browse/category/","") for s in anchor if "category" in s]
-    top = "http://careers.analytictalent.com/jobs/search/results?category%5B0%5D="
-    bot = "&format=rss"
-    url = []
-    for cat in category:
-        url.append(top+cat+url)
+class SiteSpider(scrapy.Spider):
+    """
+    spider for the site analytictalent.com
+    """
+    name = "sitespider"
+    allowed_domains = ["analytictalent.com"]
+    start_urls = (
+        'http://www.analytictalent.com/',
+        )
+    rules = (Rule(linkextractors.lxmlhtml.LxmlLinkExtractor(allow=("category" ),)
+    , callback="parse_items", follow= True),
+    )
+    def parse(self, response):
 
-    return url
-    #
+        items = []
+        item = DataScienceItem()
+        item['title'] =  response.xpath('//title/text()').extract() #title of the page
+        item['text_span'] =  response.xpath('//span/text()').extract() #span class, usually all the texts
+        item['text_li'] =  response.xpath('//li/text()').extract() #bullet points in the texts
+        items.append(item)
+
+        filename = "sitecrawl.txt"
+        with open(filename, 'wb') as f:
+            f.write(str(items))
